2d9784cc23c34dae73134837aed377ffc1c83a02
Huajun Bai <hb364@cornell.edu>
2017-09-04 15:41:24 -0400

    new class added

---
+++ src/main/java/dao/TagDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import generated.tables.records.ReceiptsRecord;
+import generated.tables.records.TagsRecord;
+import org.jooq.Configuration;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+
+import static com.google.common.base.Preconditions.checkState;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static generated.Tables.RECEIPTS;
+import static generated.Tables.TAGS;
+
+public class TagDao {
+    
+}
