ce4a74447bcc122ffeb7ea032d5f4f87b0804225
baihuajun24 <baihuajun24@gmail.com>
2017-09-26 19:28:22 -0400

    first and last annotation

--- src/main/java/controllers/ReceiptImageController.java
+++ src/main/java/controllers/ReceiptImageController.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import org.hibernate.validator.constraints.NotEmpty;
-
+import java.*;
 import static java.lang.System.out;
 
 @Path("/images")
@@ -30,12 +30,11 @@ public class ReceiptImageController {
      * https://cloud.google.com/vision/docs/detecting-fulltext
      *
      * YOU SHOULD MODIFY THIS METHOD TO RETURN A ReceiptSuggestionResponse:
-     *
-     * public class ReceiptSuggestionResponse {
-     *     String merchantName;
-     *     String amount;
-     * }
-     */
+     **/
+     public class ReceiptSuggestionResponse {
+          String merchantName;
+          String amount;
+     }
     @POST
     public ReceiptSuggestionResponse parseReceipt(@NotEmpty String base64EncodedImage) throws Exception {
         Image img = Image.newBuilder().setContent(ByteString.copyFrom(Base64.getDecoder().decode(base64EncodedImage))).build();
@@ -51,10 +50,25 @@ public class ReceiptImageController {
             // Your Algo Here!!
             // Sort text annotations by bounding polygon.  Top-most non-decimal text is the merchant
             // bottom-most decimal text is the total amount
+            
+            int maxy = -1;
+            int miny = 100000; 
             for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                out.printf("Position : %s\n", annotation.getBoundingPoly());
-                out.printf("Text: %s\n", annotation.getDescription());
+                BoundingPoly bp = annotation.getBoundingPoly();
+                ArrayList<Vertex> vList = bp.getVertices();
+                int thisY = vList[0].getY();
+                if (thisY > maxy){
+                    maxy = thisY;
+                    merchantName = annotation.getDescription();
+
+                }
+                if (thisY < miny){
+                    miny = thisY;
+                    amount = new BigDecimal(annotation.getDescription().replaceAll(',', ''));
+                }
+
             }
+
 
             //TextAnnotation fullTextAnnotation = res.getFullTextAnnotation();
             return new ReceiptSuggestionResponse(merchantName, amount);
