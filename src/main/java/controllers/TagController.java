57b05798c48bbc8f08c379436f2c2e3dd3254c9a
Huajun Bai <hb364@cornell.edu>
2017-10-06 13:04:06 -0400

    tag add to db

--- src/main/java/controllers/TagController.java
+++ src/main/java/controllers/TagController.java
@@ -3,13 +3,14 @@ package controllers;
 import api.ReceiptResponse;
 import dao.TagDao;
 import generated.tables.records.ReceiptsRecord;
+import generated.tables.records.TagsRecord;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/tags")
+
 @Consumes(MediaType.APPLICATION_JSON)
 public class TagController {
     final TagDao tags;
@@ -17,9 +18,8 @@ public class TagController {
     public TagController(TagDao tags) {
         this.tags = tags;
     }
-
+    @Path("/tags/{tag}")
     @PUT
-    @Path("/{tag}")
     public void toggleTag(@PathParam("tag") String tagLabel, int receiptId) {
         if (tags.isReceiptTagged(tagLabel, receiptId)) {
             tags.remove(tagLabel, receiptId);
@@ -28,11 +28,26 @@ public class TagController {
         }
     }
 
+    @Path("/tags/{tag}")
     @GET
-    @Path("/{tag}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<ReceiptResponse> getReceiptsWithTag(@PathParam("tag") String tagLabel) {
         List<ReceiptsRecord> receiptRecords = tags.receiptsRecordsWithTag(tagLabel);
         return receiptRecords.stream().map(ReceiptResponse::new).collect(Collectors.toList());
     }
+
+    /*@Path("/tags")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ReceiptResponse> getReceiptsWithTag(@PathParam("tag") String tagLabel) {
+        List<ReceiptsRecord> receiptRecords = tags.receiptsRecordsWithTag(tagLabel);
+        return receiptRecords.stream().map(ReceiptResponse::new).collect(Collectors.toList());
+    }
+
+    @GET
+    public List<ReceiptResponse> getReceipts() {
+        List<ReceiptsRecord> receiptRecords = receipts.getAllReceipts();
+        System.out.println("Receipt GET success!");
+        return receiptRecords.stream().map(ReceiptResponse::new).collect(toList());
+    }*/
 }
